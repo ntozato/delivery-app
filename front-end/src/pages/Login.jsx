92fd0bfe763be73c0043b218b9c4f68c9b12cfb0
hugolfsimoes <hugolfsimoes@gmail.com>
2021-11-12 07:15:17 -0300

    feat-login-fix-frontent-email-validation

--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -18,7 +18,7 @@ function Login() {
   useEffect(() => {
     const minimalPasswordLength = 6;
     // Verificação de email através do site do Stackoverflow no link https://pt.stackoverflow.com/questions/1386/express%C3%A3o-regular-para-valida%C3%A7%C3%A3o-de-e-mail
-    const emailValidationRegex = /^[_a-z0-9-]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i;
+    const emailValidationRegex = /^[_.a-z0-9-]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i;
     // Resolução do emailValidationRegex.test atrasvés do site do w3schools
     if (emailValidationRegex.test(email) && password.length >= minimalPasswordLength) {
       setIsDisable(false);
@@ -34,29 +34,29 @@ function Login() {
         <input
           type="text"
           placeholder="email@trybeer.com.br"
-          data-testId="common_login__input-email"
+          data-testid="common_login__input-email"
           onChange={ (e) => setEmail(e.target.value) }
         />
         <input
           type="password"
           placeholder="*****"
-          data-testId="common_login__input-password"
+          data-testid="common_login__input-password"
           onChange={ (e) => setPassword(e.target.value) }
         />
         <button
           type="button"
           disabled={ isDisable }
           onClick={ handleClickLogin }
-          data-testId="common_login__button-login"
+          data-testid="common_login__button-login"
         >
           LOGIN
         </button>
-        <button type="button" data-testId="common_login__button-register">
+        <button type="button" data-testid="common_login__button-register">
           Ainda não tenho conta
         </button>
       </form>
       { isError && (
-        <h1 data-testId="common_login__element-invalid-email">
+        <h1 data-testid="common_login__element-invalid-email">
           Um Erro qualquer :)
         </h1>
       ) }
