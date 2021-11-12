55a5898a577d8f15f1891774f0a4d90f12130d28
lucas <lucasportellaagu@gmail.com>
2021-11-12 18:55:59 -0300

    feat-register-error-message

--- front-end/src/components/RegisterButton.js
+++ front-end/src/components/RegisterButton.js
@@ -1,9 +1,12 @@
-import React, { useContext } from 'react';
+import React, { useState, useContext } from 'react';
+import { Navigate } from 'react-router-dom';
 import Context from '../context/Context';
 import api from '../api';
 
 const RegisterButton = () => {
   const { registerData } = useContext(Context);
+  const [redirect, setRedirect] = useState(false);
+  const [isError, setIsError] = useState(false);
   const nameMinLength = 12;
   const passwordMinLength = 6;
   const emailRegex = /^[_.a-z0-9-]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i;
@@ -13,20 +16,36 @@ const RegisterButton = () => {
   const isDisabled = !(isValidName && isEmailValid && isValidPassword);
 
   const handleSubmit = async () => {
-    const result = await api.register(registerData);
-    console.log(result);
+    try {
+      const result = await api.register(registerData);
+      if (result.statusText === 'Created') {
+        setRedirect(!redirect);
+      }
+    } catch (error) {
+      console.log('cheguei aqui');
+      setIsError(!isError);
+    }
   };
 
   return (
-    <button
-      data-testid="common_register__button-register"
-      type="button"
-      disabled={ isDisabled }
-      onClick={ handleSubmit }
-    >
-      Cadastrar
+    <div>
+      {redirect && <Navigate to="/customer/products" />}
+      <button
+        data-testid="common_register__button-register"
+        type="button"
+        disabled={ isDisabled }
+        onClick={ handleSubmit }
+      >
+        Cadastrar
 
-    </button>
+      </button>
+      { isError
+        && (
+          <p data-testid="common_register__element-invalid_register">
+            Erro ao registrar usuário
+          </p>
+        )}
+    </div>
   );
 };
 
