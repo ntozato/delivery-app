40494d24b39d67b7897b0bb452d90846038eb505
lucas <lucasportellaagu@gmail.com>
2021-11-12 16:39:01 -0300

    feat-encrypt password and fix some lint

--- back-end/src/middlewares/errorMiddleware.js
+++ back-end/src/middlewares/errorMiddleware.js
@@ -1,7 +1,6 @@
 const { StatusCodes } = require('http-status-codes');
 
-const uniqueConstraintError = (err, req, res, next) => {
-  return res.status(StatusCodes.CONFLICT).json(err.errors[0].message);
-}
+const uniqueConstraintError = (err, req, res, _next) => 
+res.status(StatusCodes.CONFLICT).json(err.errors[0].message);
 
 module.exports = { uniqueConstraintError };
--- back-end/src/services/registerService.js
+++ back-end/src/services/registerService.js
@@ -1,10 +1,13 @@
+const md5 = require('md5');
 const { User } = require('../database/models');
+
+const cryptoPassword = (password) => md5(password);
 
 const createUser = async (payload) => {
   const { name, email, password } = payload;
   const [user, created] = await User.findOrCreate({
     where: { name, email },
-    defaults: { password },
+    defaults: { password: cryptoPassword(password) },
   });
   // retornei em forma de objeto pq fica mais claro
   return { user, created };
