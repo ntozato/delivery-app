6c6f0f665a6f4724c0789b188d6d68b6d2b5a5dd
Nícolas Tozato <ntozato9@gmail.com>
2021-11-16 17:06:42 -0300

    feat-createSales-and-createSalesProducts

--- back-end/src/controllers/salesController.js
+++ back-end/src/controllers/salesController.js
@@ -3,8 +3,8 @@ const rescue = require('express-rescue');
 const service = require('../services/salesService');
 
 const createSale = rescue(async (req, res) => {
-    const payload = req.body;
-    const result = await service.createSale(payload);
+    const { saleData, products_array } = req.body;
+    const result = await service.createSale(saleData, products_array);
     res.status(StatusCodes.CREATED).json(result);
 });
 
--- back-end/src/services/salesService.js
+++ back-end/src/services/salesService.js
@@ -1,11 +1,20 @@
 const { sale } = require('../database/models');
 const { createSalesProducts } = require('./salesProductsService');
 
-const createSale = async (payload) => {
-    const { id } = await sale.create(payload); 
-    
+const createSale = async (saleData, productsArray) => {
+    const { id } = await sale.create(saleData);
+    const saleId = 'sale_id';
+    const productId = 'product_id';
+    await productsArray.map(async (product) => {
+        const payload = { 
+            [saleId]: id, [productId]: product.product_id, quantity: product.quantity,
+        };
+        await createSalesProducts(payload);
+    });
 };
 
 module.exports = {
     createSale,
 };
+
+// refatorar posteriormente para transação atômica (transaction)
