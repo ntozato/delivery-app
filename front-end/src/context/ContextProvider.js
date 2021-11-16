1c05525ead2f7eeb17551390056031ea89aa1642
lucas <lucasportellaagu@gmail.com>
2021-11-16 15:40:22 -0300

    product_id

--- front-end/src/context/ContextProvider.js
+++ front-end/src/context/ContextProvider.js
@@ -5,9 +5,9 @@ import Context from './Context';
 const ContextProvider = ({ children }) => {
   // esse testOrders é só para testar nosso componente enquanto não for possível adicionar items ao carrinho
   const testOrders = [
-    { name: 'cachaça', price: 5.50, quantity: 2 },
-    { name: 'cerveja', price: 2.30, quantity: 5 },
-    { name: 'licor', price: 32.90, quantity: 1 },
+    { product_id: 1, name: 'cachaça', price: 5.50, quantity: 2 },
+    { product_id: 2, name: 'cerveja', price: 2.30, quantity: 5 },
+    { product_id: 3, name: 'licor', price: 32.90, quantity: 1 },
   ];
 
   const detailAddressInitialState = { address: '', number: '', seller: 'vendedor 1' };
