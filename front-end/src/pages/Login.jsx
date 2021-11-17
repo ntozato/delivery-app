3a135d7019712538de6e26da35048550d4bab7a1
Fabio Marturano <fabiocmjor@gmail.com>
2021-11-17 17:35:39 -0300

    fix-orders-css

--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -8,11 +8,11 @@ function Login() {
   const { setUserEmail } = useContext(Context);
   const [isError, setIsError] = useState(false);
   const [loginOk, setLoginOk] = useState(false);
-  const [email, setEmail] = useState('zebirita@email.com');
-  const [password, setPassword] = useState('$#zebirita#$');
-  /*
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  /*
+  const [email, setEmail] = useState('zebirita@email.com');
+  const [password, setPassword] = useState('$#zebirita#$');
   */
   const [redirect, setRedirect] = useState(false);
 
