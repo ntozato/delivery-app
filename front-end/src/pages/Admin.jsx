3c8ea47f22b6346169519986da63b1c4c4619bc0
hugolfsimoes <hugolfsimoes@gmail.com>
2021-11-19 19:21:18 -0300

    feat-admin-clean-form

--- front-end/src/pages/Admin.jsx
+++ front-end/src/pages/Admin.jsx
@@ -4,12 +4,13 @@ import api from '../api/index';
 // import { Navigate } from 'react-router-dom';
 
 function Admin() {
-  const [newUserData, setNewUserData] = useState({
+  const dafaultUserData = {
     name: '',
     email: '',
     password: '',
     role: 'seller',
-  });
+  };
+  const [newUserData, setNewUserData] = useState(dafaultUserData);
 
   const handleChangeNewUser = ({ target: { name, value } }) => {
     const aux = { ...newUserData };
@@ -17,40 +18,61 @@ function Admin() {
     setNewUserData(aux);
   };
 
+  const handleCLickSendNewUser = async () => {
+    try {
+      await api.register(newUserData);
+      setNewUserData(dafaultUserData);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="Admin">
       <NavBar />
       <form>
         <input
+          data-testid="admin_manage__input-name"
           type="text"
           name="name"
           placeholder="Nome Sobrenome"
+          value={ newUserData.name }
           onChange={ (e) => handleChangeNewUser(e) }
         />
 
         <input
+          data-testid="admin_manage__input-email"
           type="text"
           name="email"
           placeholder="seu-email@site.com.br"
+          value={ newUserData.email }
           onChange={ (e) => handleChangeNewUser(e) }
         />
 
         <input
+          data-testid="admin_manage__input-password"
           type="password"
           name="password"
           placeholder="******"
+          value={ newUserData.password }
           onChange={ (e) => handleChangeNewUser(e) }
         />
 
-        <select name="role" onChange={ (e) => handleChangeNewUser(e) }>
+        <select
+          data-testid="admin_manage__select-role"
+          name="role"
+          value={ newUserData.role }
+          onChange={ (e) => handleChangeNewUser(e) }
+        >
           <option value="seller" selected>Vendedor</option>
           <option value="customer">Cliente</option>
           <option value="administrator">Administrador</option>
         </select>
 
         <button
+          data-testid="admin_manage__button-register"
           type="button"
-          onClick={ () => api.register(newUserData) }
+          onClick={ handleCLickSendNewUser }
         >
           Cadastrar
         </button>
