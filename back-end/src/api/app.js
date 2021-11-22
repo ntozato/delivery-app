3606acfe78dac3424752247df976833709b13cd4
lucas <lucasportellaagu@gmail.com>
2021-11-22 16:37:56 -0300

    feat socket basic structure

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
+
+const app = express();
+const http = require('http').createServer(app);
+
+const io = require('socket.io')(http, {
+    cors: {
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    },
+});
+
 const { registerRoute,
     loginRoute,
     custumerRoute,
@@ -11,9 +22,10 @@ const { registerRoute,
 } = require('../routes/index');
 const { uniqueConstraintError, genericError } = require('../middlewares/errorMiddleware');
 
-const app = express();
 app.use(express.json());
 app.use(cors());
+
+require('../sockets/checkout')(io);
 
 app.use('/register', registerRoute);
 app.use('/login', loginRoute);
@@ -26,4 +38,4 @@ app.use('/products', productRoute);
 
 app.use(uniqueConstraintError, genericError);
 
-module.exports = app;
+module.exports = http;
